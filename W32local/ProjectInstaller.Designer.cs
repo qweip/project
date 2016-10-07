c49d805a324da71118cec32f1b6851fe450dc5a4
wei <qweip88989@gmail.com>
2016-10-07 16:14:56 +0800

    Initial commit.

---
+++ W32local/ProjectInstaller.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace W32local
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "W32Local Description";
+            this.serviceInstaller1.DisplayName = "W32Local";
+            this.serviceInstaller1.ServiceName = "W32Local";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
---
+++ W32local/Service1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace W32local
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            this.AutoLog = false;
+            
+            
+            if (!EventLog.SourceExists("MySource"))
+            {
+                EventLog.CreateEventSource(
+                "MySource", "MyLog");
+            }
+            eventLog1.Source = "MySource";
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ProcessStartInfo p = new ProcessStartInfo();
+            DateTime.Now.ToShortTimeString();
+            DateTime dt = DateTime.Now;
+            eventLog1.WriteEntry("Active: active (running) since " + dt.DayOfWeek.ToString() + dt.ToLocalTime() + " " + dt.Kind.ToString());
+            try
+            {  
+                p.FileName = "W32local1.exe";
+                p.WorkingDirectory = @"C:\WService";
+                Process.Start(p);
+            }
+            catch (Exception e)
+            {
+                eventLog1.WriteEntry(e.Message);
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+            DateTime.Now.ToShortTimeString();
+            DateTime dt = DateTime.Now;
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("W32local1"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch(Exception e)
+            {
+                eventLog1.WriteEntry(e.Message);
+            }
+            eventLog1.WriteEntry("Active: active (stop) since " + dt.DayOfWeek.ToString() + dt.ToLocalTime() + " " + dt.Kind.ToString());
+        }
+    }
+}
